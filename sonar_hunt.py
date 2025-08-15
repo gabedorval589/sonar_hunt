6213672b048759b263b7dda10c09208d395a6184
Gabe Dorval <gabedorval589@gmail.com>
2025-08-15 08:23:02 -0700

    Imported three modules, created get_new_board().

--- sonar_hunt.py
+++ sonar_hunt.py
@@ -3,3 +3,17 @@
 import random
 import sys
 import math
+
+def get_new_board():
+    """ Create a new 60x15 board data structure."""
+    board = []
+    for x in range(60): # The main list is a list of 60 lists."""
+        board.append([])
+        for y in range(15): 
+            """Each list in the main list has 15 single-character strings."""
+            """Use different characters for the ocean to make it more readable."""
+            if random.randint(0, 1) == 0:
+                board[x].append('~')
+            else:
+                board[x].append('`')
+    return board
